8e5ca27d052932b3ee193c66cc3a3b373c7d83d4
PseudoscienceGames <quade.81@gmail.com>
2018-01-13 12:58:03 -0600

    Added GridLocs

---
+++ SimpleTBS/Assets/Scripts/GridController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+	public GameObject gridLoc;
+	public List<GridLoc> grid = new List<GridLoc>();
+	public Mesh gridMesh;
+
+	private void Start()
+	{
+		int index = 0;
+		foreach(Vector3 p in gridMesh.vertices)
+		{
+			GridLoc g = Instantiate(gridLoc, p, Quaternion.identity).GetComponent<GridLoc>();
+			g.index = index;
+			index++;
+			g.pos = p;
+			g.transform.parent = transform;
+		}
+	}
+}
